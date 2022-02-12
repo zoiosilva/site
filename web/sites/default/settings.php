d4fcf5f6182fef334a083c5f68dcc21ab2df548e
Rafael "Zoio" Silva <zoiosilva@yahoo.com.br>
2022-02-12 12:24:08 -0400

    Fixing database connection.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -89,10 +89,10 @@
  * @endcode
  */
 $databases['default']['default'] = [
-  'database' => 'site',
-  'username' => parse_url(getenv('STACKHERO_MARIADB_USER')),
-  'password' => parse_url(getenv('STACKHERO_MARIADB_PASSWORD')),
-  'host' => parse_url(getenv('STACKHERO_MARIADB_HOST')),
+  'database' => getenv('STACKHERO_MARIADB_DATABASE'),
+  'username' => getenv('STACKHERO_MARIADB_USER'),
+  'password' => getenv('STACKHERO_MARIADB_PASSWORD'),
+  'host' => getenv('STACKHERO_MARIADB_HOST'),
   'port' => '3306',
   'driver' => 'mysql',
   'prefix' => '',
